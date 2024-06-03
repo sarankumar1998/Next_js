992729f7142749b6996ef6063b4134b5ae4e1fc5
nithyakalyani <neodev2@nipponpaint.co.in>
2024-06-03 12:43:43 +0530

    new cors add

---
+++ app/lib/cors.js
@@ -0,0 +1,14 @@
+export function enableCors(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*'); // or specify your domain here
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+  }
+  
